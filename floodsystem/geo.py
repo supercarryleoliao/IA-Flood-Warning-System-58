9ba59a1dc05ea44811fd6500daa711b4767198bd
Leo Liao <zl497@cam.ac>
2022-02-08 18:29:45 +0000

    deleted pip install command

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,19 +5,19 @@
 geographical data.
 
 """
-pip install haversine
+
 from turtle import distance
-import haversine
 from .utils import sorted_by_key  # noqa
 def stations_by_distance(stations,p):
-    ''''''return a list of tuples containing stations and distances''''''
+    """return a list of tuples containing stations and distances"""
     list0=[]
-    tuple0=(,)
+    tuple0=(0,0,0)
     for i in range (len(stations)):
         tuple0[0]=stations[i].name
+        tuple0[1]=stations[i].town
         coord=stations[i].coord
         distance=haversine(p,coord)
-        tupe0[1]=distance
+        tuple0[2]=distance
         list0+=tuple0
     return list0
          
