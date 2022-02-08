edc240930489a88ddf5a2abbe820363a54c0a527
Leo Liao <zl497@cam.ac>
2022-02-08 21:16:45 +0000

    Added test_Task1F

---
+++ test_Task1F.py
@@ -0,0 +1,6 @@
+"""Unit test for Task 1F"""
+import Task1F
+def test_Task1F():
+    list1=run()
+    assert list1==['Addlestone', 'Airmyn', 'Allerford', 'Arundel Queen St Bridge', 'Blacktoft', 'Braunton', 'Brentford', 'Broomfleet Weighton Lock', 'East Hull Hedon Road', 'Eccelsfield Morrisons', 'Fleetwood', 'Goole', 'Gravesend', 'Hedon Thorn Road Bridge', 'Hedon Westlands Drain', 'Hull Barrier Victoria Pier', 'Hull High Flaggs, Lincoln Street', "King's Lynn", 'Littlehampton', 'Paull', 'Salt end', 'Silloth Docks', 'Stone Creek', 'Templers Road', 'Topsham', 'Totnes', 'Truro Harbour', 'Weare Giffard', 'Westbrook Mill', 'Wilfholme PS', 'Wilfholme PS Hull Level']
+    
