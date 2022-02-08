6dd6609104f85fe3826b3c5534b6307a9809395e
Leo Liao <zl497@cam.ac>
2022-02-08 21:14:01 +0000

    Added Task1F

---
+++ Task1F.py
@@ -0,0 +1,11 @@
+from floodsystem.station import inconsistent_typical_range_stations
+from floodsystem.stationdata import build_station_list
+def run():
+    """"return a list of station names, in alphabetical order, for stations with inconsistent data"""
+    stations=build_station_list
+    list1=inconsistent_typical_range_stations(stations)
+    list1.sort()
+    return list1
+if __name__ == "__main__":
+    print("*** Task 1F: CUED Part IA Flood Warning System ***")
+    run()
