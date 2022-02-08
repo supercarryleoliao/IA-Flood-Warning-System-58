03341e8888406e734fbae5ba56e672e9156892a6
Leo Liao <zl497@cam.ac>
2022-02-08 19:53:40 +0000

    added task1b

--- Task1B.py
+++ Task1B.py
@@ -13,5 +13,5 @@ def run():
     return list1
 
 if __name__ == "__main__":
-    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
     run()
