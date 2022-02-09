958e7a37f5a7c7d8341800648f8d5daa5341d832
Leo Liao <zl497@cam.ac.uk>
2022-02-09 21:17:28 +0000

    debug

--- Task1B.py
+++ Task1B.py
@@ -1,6 +1,7 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.geo import stations_by_distance
 from floodsystem.utils import sorted_by_key
+import haversine
 def run():
     """Requirements for Task 1B"""
 
@@ -9,8 +10,8 @@ def run():
     coord_of_Cambridgecitycentre=(52.2053,0.1218)
 
     list1=stations_by_distance(stations,coord_of_Cambridgecitycentre)
-    sorted_by_key(list1, 2)
-    return list1
+    list2=sorted_by_key(list1, 2,reverse=False)
+    return list2
 
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
