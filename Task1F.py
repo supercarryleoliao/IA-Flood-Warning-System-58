a3724d8e333fdb2e7512d22e3617c1fe20cf8429
Leo Liao <zl497@cam.ac.uk>
2022-02-09 19:45:58 +0000

    debug

--- Task1F.py
+++ Task1F.py
@@ -2,7 +2,7 @@ from floodsystem.station import inconsistent_typical_range_stations
 from floodsystem.stationdata import build_station_list
 def run():
     """"return a list of station names, in alphabetical order, for stations with inconsistent data"""
-    stations=build_station_list
+    stations=build_station_list()
     list1=inconsistent_typical_range_stations(stations)
     list1.sort()
     return list1
