107260afac8322e134ed057876f496a29e07ec38
Leo Liao <zl497@cam.ac.uk>
2022-02-09 21:17:55 +0000

    debug

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,17 +6,13 @@ geographical data.
 
 """
 from haversine import haversine, Unit
-from turtle import distance
-import haversine
 from .utils import sorted_by_key  # noqa
 
 def stations_by_distance(stations,p):
     #return a list of tuples containing stations and distances
     list0=[]
     for station in stations:
-        coord=station.coord
-        distance=haversine(p,coord)
-        tuple0=(station.name,station.town,distance)
+        tuple0=(station.name,station.town,haversine(p,station.coord))
         list0.append(tuple0)
     return list0
 def stations_within_radius(stations, centre, r):
