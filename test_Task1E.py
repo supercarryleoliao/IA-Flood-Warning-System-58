f48cff34edeb0edb473f6b45c5bd4b9e3b14331b
Eugene Lin <el551@cam.ac.uk>
2022-02-10 11:54:44 +0000

    changed assertions in test_Task1E

--- test_Task1E.py
+++ test_Task1E.py
@@ -2,4 +2,15 @@ from floodsystem.geo import rivers_by_station_number
 from floodsystem.stationdata import build_station_list 
 def test_Task1E():
     stations=build_station_list()
-    assert rivers_by_station_number(stations, 9) == [('River Thames', 55), ('River Avon', 31), ('River Great Ouse', 30), ('River Derwent', 25), ('River Aire', 24), ('River Calder', 23), ('River Stour', 21), ('River Severn', 21), ('River Colne', 18), ('River Ouse', 18)]
+    rivers = rivers_by_station_number(stations, 9)
+    assert rivers[0][1] == 55
+    assert rivers[1][1] == 31
+    assert rivers[2][1] == 30
+    assert rivers[3][1] == 25
+    assert rivers[4][1] == 24
+    assert rivers[5][1] == 23
+    assert rivers[6][1] == 21
+    assert rivers[7][1] == 21
+    assert rivers[8][1] == 18
+    assert rivers[9][1] == 18
+    assert len(rivers) == 10
