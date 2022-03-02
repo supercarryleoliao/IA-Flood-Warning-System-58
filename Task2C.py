400a805f87d78cf95ef422db2d251d0551e02c7a
Eugene Lin <el551@cam.ac.uk>
2022-03-02 22:24:39 +0000

    task2c function

---
+++ Task2C.py
@@ -0,0 +1,5 @@
+#Demonstration program to show completeness of Task 2C
+from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_highest_rel_level
+def run():
+    stations = build_station_list()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -7,3 +7,8 @@ def stations_level_over_threshold(stations, tol): #2B part 2
                 stationList.append((station.name, station.relative_water_level()))
     sortedList = sorted(stationList, key=lambda x: x[1], reverse=True)
     return sortedList
+
+def stations_highest_rel_level(stations, N):
+    stationList = stations_level_over_threshold(stations, 0)
+    Nslice = stationList[:N]
+    return Nslice
