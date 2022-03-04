1e8be0703d31bf9a452e21d9fa3491e58fd542e2
Leo Liao <zl497@cam.ac.uk>
2022-03-04 01:03:16 +0000

    output tuple with first element being station object

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -4,7 +4,7 @@ def stations_level_over_threshold(stations, tol): #2B part 2
     for station in stations:
         if station.typical_range_consistent() and station.latest_level != None:
             if station.relative_water_level() > tol:
-                stationList.append((station.name, station.relative_water_level()))
+                stationList.append((station, station.relative_water_level()))
     sortedList = sorted(stationList, key=lambda x: x[1], reverse=True)
     return sortedList
 
@@ -12,7 +12,7 @@ def stations_highest_rel_level(stations, N):
     stationList = list()
     for station in stations:
         if station.typical_range_consistent() and station.latest_level != None:
-            stationList.append((station.name, station.relative_water_level()))
+            stationList.append((station, station.relative_water_level()))
     sortedList = sorted(stationList, key=lambda x: x[1], reverse=True)
     Nslice = sortedList[0:N]
     return Nslice
