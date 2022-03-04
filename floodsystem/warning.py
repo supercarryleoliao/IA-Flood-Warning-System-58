b808b550a6647c66e2f4347dec0f5c1126f6ac66
Eugene Lin <el551@cam.ac.uk>
2022-03-04 11:07:32 +0000

    push for markup

--- floodsystem/warning.py
+++ floodsystem/warning.py
@@ -1,4 +1,10 @@
 #Submodule containing 2G flood warning assessment function
+import matplotlib
+from scipy.misc import derivative
+import numpy as np
+from datetime import date
+def trend(poly, d0):
+    return derivative(poly, matplotlib.dates.date2num(date.today()))
 
 def warning(stations):
     levelList = list()
@@ -7,12 +13,12 @@ def warning(stations):
         level = ""
         if s.typical_range_consistent() == False or s.latest_level == None:
             level = "Inconclusive"
-        elif s.relative_water_level > 1 and trend > 1: #update this once Leo's code arrives for trend calculations
+        elif s.relative_water_level > 1 and trend() > 0: #update this once Leo's code arrives for trend calculations
             level = "Severe"
             severeList.append(s.town)
-        elif s.relative_water_level > 1 or trend > 1: #update this once Leo's code arrive for trend calculations
+        elif s.relative_water_level > 1 or trend > 0: #update this once Leo's code arrives for trend calculations
             level = "High"
-        elif s. relative_water_level > 0.7:
+        elif s.relative_water_level > 0.8:
             level = "Moderate"
         else:
             level = "Low"
